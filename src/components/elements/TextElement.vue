4a19dab0ff5c9696bfcc104ec5f9d2eb2f391621
tooolbox <tooolbox@users.noreply.github.com>
2020-01-25 22:06:58 -0800

    Support "examples" as well as "example" for text field placeholder.

--- src/components/elements/TextElement.vue
+++ src/components/elements/TextElement.vue
@@ -1,6 +1,6 @@
 <template>
   <b-field :label="schema.title" :message="schema.description">
-    <b-input :value="value" @input="$emit('input', $event)" :placeholder="schema.example">
+    <b-input :value="value" @input="$emit('input', $event)" :placeholder="schema.example || schema.examples && schema.examples.length && schema.examples[0]">
     </b-input>
   </b-field>
 </template>
